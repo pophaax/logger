3bc4bb7185b515260b5fcc82e065034b47b84b36
Natalie Josefsson <natalie.josefsson@ha.ax>
2015-07-21 09:45:34 +0300

    cleaned includes in header files

--- Logger.cpp
+++ Logger.cpp
@@ -9,6 +9,12 @@
 
 #include "Logger.h"
 #include <iostream>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/support/date_time.hpp>
 
 
 namespace logging = boost::log;
--- Logger.h
+++ Logger.h
@@ -7,16 +7,10 @@
 
 #ifndef LOGGER_LOGGER_H_
 #define LOGGER_LOGGER_H_
+
 #include <string>
-#include <sstream>
-#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
 #include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/support/date_time.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
 
 BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(global_logger,
